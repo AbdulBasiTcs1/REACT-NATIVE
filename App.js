4744250ec6586b219481f2d2923306351515a334
AbdulBasiTcs1 <bearnwith@gmail.com>
2026-02-23 12:08:19 +0500

    building with the react native

--- App.js
+++ App.js
@@ -3,18 +3,34 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.wrapper}>
+    <View style={styles.box1}>
+      <Text >This is Header of the App</Text>
+    </View>
+    <View style={styles.box2}>
+      <Text>This is the Footer of the App</Text>
+    </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  wrapper: {
+    backgroundColor:'lightblue',
+    flex :1,
+    justifyContent:'center',
+    textAlign:'center'
   },
+  box1:{
+    backgroundColor:'white',
+    flex:2,
+    justifyContent:'center',
+    textAlign:'center'
+  },
+  box2:{
+    backgroundColor:'pink',
+    flex:3,
+    justifyContent:'center',
+    textAlign:'center'
+  }
 });
